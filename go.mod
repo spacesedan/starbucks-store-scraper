549df5ea6f8e8e64f3d2ea93289bcd251772dd01
Jose Duarte <jduarte0912@gmail.com>
2023-05-29 22:13:10 -0700

    version 1 of starbucks store scraper

---
+++ go.mod
@@ -0,0 +1,31 @@
+module starbucsk-store-scraper
+
+go 1.20
+
+require github.com/gocolly/colly/v2 v2.1.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/antchfx/htmlquery v1.3.0 // indirect
+	github.com/antchfx/xmlquery v1.3.15 // indirect
+	github.com/antchfx/xpath v1.2.4 // indirect
+	github.com/go-rod/rod v0.113.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jawher/mow.cli v1.2.0 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/ysmood/fetchup v0.2.3 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/got v0.34.1 // indirect
+	github.com/ysmood/gson v0.7.3 // indirect
+	github.com/ysmood/leakless v0.8.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/go-rod/rod"
+)
+
+type Store struct {
+	Address           string `json:"address"`
+	StoreLocationLink string `json:"storeLocationLink"`
+	PhoneNumber       string `json:"phoneNumber"`
+}
+
+func getStores(link string) map[string]Store {
+	page := rod.New().MustConnect().MustPage(link)
+	defer page.MustClose()
+
+	locationList := page.MustElement(".base___3sH_T")
+	locations := locationList.MustElements(".base___3LiS9")
+
+	stores := make(map[string]Store)
+	for _, location := range locations[:33] {
+		address := location.MustElement("[data-e2e=address]").MustText()
+		storelink := location.MustElement("[data-e2e=cardLink]").MustProperty("href")
+
+		stores[address] = Store{
+			StoreLocationLink: storelink.Str(),
+			Address:           address,
+		}
+
+	}
+	return stores
+}
+
+func main() {
+	var linkVar string
+
+	flag.StringVar(&linkVar, "link", "link", "link used to scrape for stores")
+	flag.Parse()
+
+	if linkVar == "" {
+		log.Fatal("Could not start scrape without link")
+	}
+	browser := rod.New().MustConnect()
+	defer browser.MustClose()
+
+	pool := rod.NewPagePool(5)
+
+	create := func() *rod.Page {
+		return browser.MustIncognito().MustPage()
+	}
+
+	log.Println("Starting scrape")
+	stores := getStores(linkVar)
+
+	getPhoneNumber := func(store Store) Store {
+		page := pool.Get(create)
+		defer pool.Put(page)
+
+		page.Timeout(5 * time.Second).MustNavigate(store.StoreLocationLink).MustWaitOpen()
+
+		storeInformation := page.MustElement("[aria-labelledby=expandedLocationCardLabel]")
+		contactInformation := storeInformation.MustElement(".whiteSpace-noWrap")
+		phoneNumber := contactInformation.MustElement("a").MustProperty("href").Str()
+		phoneNumber = strings.TrimPrefix(phoneNumber, "tel:")
+		store.PhoneNumber = phoneNumber
+		return store
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(len(stores))
+	for k, v := range stores {
+		go func(v Store, k string) {
+			defer wg.Done()
+			v = getPhoneNumber(v)
+			stores[k] = v
+		}(v, k)
+	}
+
+	wg.Wait()
+	pool.Cleanup(func(p *rod.Page) {
+		p.MustClose()
+	})
+
+	var storesArray []Store
+
+	for _, store := range stores {
+		storesArray = append(storesArray, store)
+	}
+
+	fileName := fmt.Sprintf("stores/stores_%v,json", time.Now().Unix())
+	file, _ := json.MarshalIndent(storesArray, "", " ")
+	_ = os.WriteFile(fileName, file, 0644)
+	log.Printf("Scrape finished wrote file to stores/%v", fileName)
+}
